63776f7a9b25366eeffbbbf9df22049bf61e6781
Nasr Al-Rahbi <nasr@abom.me>
2023-01-01 21:56:34 +0400

    Home screen done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,19 +11,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.blue[800],
+      backgroundColor: Color(0xff2471B7),
       bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.chat_bubble), label: 'Chats'),
+              icon: SvgPicture.asset(
+                'assets/home-solid.svg',
+                color: Color(0xff2471B7),
+                width: 30,
+                height: 30,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/user-solid.svg',
+                color: Color(0xff9ba3b8),
+                width: 30,
+                height: 30,
+              ),
+              label: 'Profile'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/settings-solid.svg',
+                color: Color(0xff9ba3b8),
+                width: 30,
+                height: 30,
+              ),
+              label: 'Chats'),
         ],
       ),
       body: SafeArea(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          // padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Stack(
             children: [
               /// Top Header
@@ -31,8 +57,9 @@ class _HomePageState extends State<HomePage> {
                 left: 0,
                 right: 0,
                 child: Container(
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  height: size.height * 0.4,
+                  width: size.width,
                   child: Column(
                     children: [
                       Row(
@@ -55,20 +82,22 @@ class _HomePageState extends State<HomePage> {
                               Text(
                                 "1 jan, 2023",
                                 style: TextStyle(
-                                    color: Colors.blue[200],
+                                    color: Colors.white.withOpacity(0.5),
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold),
                               )
                             ],
                           ),
                           Container(
-                            padding: EdgeInsets.all(15),
+                            padding: EdgeInsets.all(13),
                             decoration: BoxDecoration(
-                                color: Colors.blue[600],
+                                color: Color(0xff2684d6),
                                 borderRadius: BorderRadius.circular(15)),
-                            child: const Icon(
-                              Icons.notifications,
+                            child: SvgPicture.asset(
+                              'assets/notification.svg',
                               color: Colors.white,
+                              width: 30,
+                              height: 30,
                             ),
                           )
                         ],
@@ -79,7 +108,7 @@ class _HomePageState extends State<HomePage> {
                       Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
-                            color: Colors.blue[600],
+                            color: Color(0xff2684d6).withOpacity(0.7),
                             borderRadius: BorderRadius.circular(11)),
                         alignment: Alignment.center,
                         child: Row(
@@ -132,7 +161,282 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-              )
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(50),
+                            topLeft: Radius.circular(50))),
+                    width: size.width,
+                    height: size.height * 0.5,
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 7,
+                            decoration: BoxDecoration(
+                                color: Color(0xff9084d6).withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                          SizedBox(
+                            height: 35,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Lets, help you",
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Icon(
+                                Icons.more_horiz,
+                                color: Colors.black87.withOpacity(0.7),
+                                size: 30,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 35,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 15),
+                            width: size.width,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black.withOpacity(0.04),
+                                      blurRadius: 15,
+                                      spreadRadius: 15,
+                                      offset: Offset(10, 10))
+                                ]),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(15),
+                                      height: 70,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(17),
+                                        color: Colors.deepOrangeAccent,
+                                      ),
+                                      child: SvgPicture.asset(
+                                        'assets/chat-text-solid.svg',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Chat",
+                                          style: TextStyle(
+                                              color: Colors.black87
+                                                  .withOpacity(0.7),
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          "Chatting with psychiatrist",
+                                          style: TextStyle(
+                                              color:
+                                                  Colors.black.withOpacity(0.3),
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.black.withOpacity(0.3),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 15),
+                            width: size.width,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black.withOpacity(0.04),
+                                      blurRadius: 15,
+                                      spreadRadius: 15,
+                                      offset: Offset(10, 10))
+                                ]),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(15),
+                                      height: 70,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(17),
+                                        color: Colors.deepPurple,
+                                      ),
+                                      child: SvgPicture.asset(
+                                        'assets/calander-solid.svg',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Booking Appointment",
+                                          style: TextStyle(
+                                              color: Colors.black87
+                                                  .withOpacity(0.7),
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          "Appointment with a psychiatrist",
+                                          style: TextStyle(
+                                              color:
+                                                  Colors.black.withOpacity(0.3),
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.black.withOpacity(0.3),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 15),
+                            width: size.width,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black.withOpacity(0.04),
+                                      blurRadius: 15,
+                                      spreadRadius: 15,
+                                      offset: Offset(10, 10))
+                                ]),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(15),
+                                      height: 70,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(17),
+                                        color: Colors.teal,
+                                      ),
+                                      child: SvgPicture.asset(
+                                        'assets/open-book-solid.svg',
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Advices",
+                                          style: TextStyle(
+                                              color: Colors.black87
+                                                  .withOpacity(0.7),
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          "Read advices help you",
+                                          style: TextStyle(
+                                              color:
+                                                  Colors.black.withOpacity(0.3),
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.black.withOpacity(0.3),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ))
             ],
           ),
         ),
@@ -148,7 +452,8 @@ class _HomePageState extends State<HomePage> {
           width: 60,
           height: 60,
           decoration: BoxDecoration(
-              color: Colors.blue[600], borderRadius: BorderRadius.circular(15)),
+              color: Color(0xff2684d6),
+              borderRadius: BorderRadius.circular(15)),
           child: Text(
             emoji,
             style: TextStyle(fontSize: 30),
@@ -159,7 +464,7 @@ class _HomePageState extends State<HomePage> {
         ),
         Text(
           text,
-          style: TextStyle(color: Colors.white, fontSize: 17),
+          style: const TextStyle(color: Colors.white, fontSize: 17),
         )
       ],
     );
