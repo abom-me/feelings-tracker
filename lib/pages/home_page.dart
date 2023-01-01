1911bf37837334a14473ccd2765752336d8c5223
Nasr Al-Rahbi <nasr@abom.me>
2023-01-01 20:18:39 +0400

    Top Header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,84 +11,157 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[800],
+      backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble), label: 'Chats'),
+        ],
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Stack(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              /// Top Header
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
                     children: [
-                      const Text(
-                        "Hi, Nasr!",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Hi, Nasr!",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "1 jan, 2023",
+                                style: TextStyle(
+                                    color: Colors.blue[200],
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                                color: Colors.blue[600],
+                                borderRadius: BorderRadius.circular(15)),
+                            child: const Icon(
+                              Icons.notifications,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius: BorderRadius.circular(11)),
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              "Search",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
                       ),
                       const SizedBox(
-                        height: 8,
+                        height: 25,
                       ),
-                      Text(
-                        "1/jan/23",
-                        style: TextStyle(
-                            color: Colors.teal[300],
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      )
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            'How Do You Feel?',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          emoji('😞', 'Bad'),
+                          emoji('☺️', 'Fine'),
+                          emoji('😢', 'Sad'),
+                          emoji('😁', 'Happy'),
+                        ],
+                      ),
                     ],
                   ),
-                  Container(
-                    padding: EdgeInsets.all(7),
-                    decoration: BoxDecoration(
-                        color: Colors.teal[600],
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(11)),
-                alignment: Alignment.center,
-                child: Row(
-                  children: const [
-                    Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      "Search",
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
                 ),
-              ),
-              Row(
-                children: [
-                  Text(data)
-                ],
               )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Column emoji(String emoji, String text) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+              color: Colors.blue[600], borderRadius: BorderRadius.circular(15)),
+          child: Text(
+            emoji,
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        SizedBox(
+          height: 7,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 17),
+        )
+      ],
     );
   }
 }
